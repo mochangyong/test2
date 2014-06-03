fec095950084e3198affc63474634350b1e80cdd
lee yunhee <lee yunhee@my_computer>
2014-06-03 15:53:29 +0900

    bean

---
+++ Test2/src/BeanTest.java
@@ -0,0 +1,13 @@
+
+public class BeanTest {
+
+	private String name ="himggildong";
+	
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	
+}
